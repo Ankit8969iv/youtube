7ea13d614d2809580bac38dca560a3da1e282d60
Ankit8969iv <ankit.yadav@interviewvector.tech>
2022-02-27 15:37:44 +0530

    making chess board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import './index.css'
 
-function App() {
+const App = () => {
+  const [dimension, setDimension] = useState(0);
+  const [chess, setChess] = useState([]);
+
+  const black = {
+    width:"100px",
+    height:"100px",
+    backgroundColor:'black'
+  }
+  const white = {
+    width:"100px",
+    height:"100px",
+    backgroundColor:'white'
+  }
+  const chessBox = {
+    width:100*dimension,
+    display:'flex',
+    flexWrap:'wrap',
+    marginTop:"20px",
+    boxShadow:`0px 10px 10px rgba(0,0,0,0.1)`
+  }
+
+  const makeChessBoard = ()=>{
+    let arr = [];
+
+    for (let i=0;i<dimension;i++){
+      let temp = [];
+      for (let j=0;j<dimension;j++){
+        if ((i+j)%2)
+          temp.push(<div style={black}></div>);
+        else  
+          temp.push(<div style={white}></div>)
+      }
+      arr.push(temp);
+    }
+
+    setChess(arr);
+  }
+
+  useEffect(()=>{
+    makeChessBoard();
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='chess'>
+      
+      <div>
+        <h2>
+          <span>N x N </span> 
+          ChessBoard
+        </h2>
+        <input type="number" placeholder='Enter the Dimension' onChange={(e)=>setDimension(e.target.value)} />
+      </div>
+
+      <section style={chessBox}>
+        {chess}
+      </section>
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
